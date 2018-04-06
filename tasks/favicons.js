c657e4f05c308877b3e0276c71f96aec6883f1ad
Pieter Beulque <pieterbeulque@gmail.com>
2018-04-06 11:57:51 +0200

    Add background-color support to touch icon generation

--- tasks/favicons.js
+++ tasks/favicons.js
@@ -1,40 +1,95 @@
 const gulp = require('gulp');
 
-const resize = require('gulp-image-resize');
+// const resize = require('gulp-image-resize');
+const gm = require('gulp-gm');
 const imagemin = require('gulp-imagemin');
 const rename = require('gulp-rename');
 
 const sizes = {
-	'android-chrome-192x192': 192,
-	'apple-touch-icon-120x120': 120,
-	'apple-touch-icon-152x152': 152,
-	'apple-touch-icon-167x167': 167,
-	'apple-touch-icon-180x180': 180,
+	'android-chrome-192x192': {
+		width: 192,
+		transparent: false,
+	},
+	'apple-touch-icon-120x120': {
+		width: 120,
+		transparent: false,
+	},
+	'apple-touch-icon-152x152': {
+		width: 152,
+		transparent: false,
+	},
+	'apple-touch-icon-167x167': {
+		width: 167,
+		transparent: false,
+	},
+	'apple-touch-icon-180x180': {
+		width: 180,
+		transparent: false,
+	},
 	'favicon-16x16': 16,
 	'favicon-32x32': 32,
 	'favicon-96x96': 96,
 	'favicon-512x512': 512,
-	largetile: 310,
-	mediumtile: 270,
-	smalltile: 70,
-	widetile: [310, 150],
+	largetile: {
+		width: 310,
+		transparent: false,
+	},
+	mediumtile: {
+		width: 270,
+		transparent: false,
+	},
+	smalltile: {
+		width: 70,
+		transparent: false,
+	},
+	widetile: {
+		width: 310,
+		height: 150,
+		transparent: false,
+	},
 };
 
 module.exports = (config) => {
-	const { src, dest } = config;
+	const { src, dest, backgroundColor = '#ffffff', padding = 0.825 } = config;
 
-	const resizers = Object.entries(sizes).map(([filename, size]) => {
-		const parsedSize = Array.isArray(size) ? size : [size, size];
-		const [width, height] = parsedSize;
+	const resizers = Object.entries(sizes).map(([filename, options]) => {
+		const settings = {
+			width: 512,
+			height: 512,
+			transparent: true,
+		};
+
+		if (typeof options === 'number') {
+			Object.assign(settings, {
+				width: options,
+				height: options,
+			});
+		} else {
+			const { width, height, transparent } = options;
+
+			Object.assign(settings, {
+				width,
+				height: height || width,
+				transparent: !!transparent,
+			});
+		}
 
 		const task = () => gulp
 			.src(src)
-			.pipe(resize({
-				width,
-				height,
-				crop: true,
-				cover: false,
-				format: 'png',
+			.pipe(gm((favicon) => {
+				const { width, height, transparent } = settings;
+
+				const shortest = width <= height ? width : height;
+
+				if (transparent) {
+					return favicon.resize(width, height);
+				}
+
+				return favicon
+					.resize(shortest * padding, shortest * padding)
+					.gravity('Center')
+					.extent(width, height)
+					.background(backgroundColor);
 			}))
 			.pipe(imagemin())
 			.pipe(rename(`${filename}.png`))
