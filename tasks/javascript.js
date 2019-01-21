7b4d5a8451e94a4fa85bc90d690be7d832c513c9
Simon Menke <simon.menke@gmail.com>
2019-01-21 17:25:35 +0100

    wip

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,70 +1,107 @@
 const gulp = require('gulp');
 
-const babelify = require('babelify');
-const babelPresetEnv = require('babel-preset-env');
-const babili = require('gulp-babel-minify');
-const browserify = require('browserify');
+const filter = require('gulp-filter');
+const minify = require("gulp-babel-minify");
 const buffer = require('vinyl-buffer');
 const rename = require('gulp-rename');
+const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
 const resolve = require('rollup-plugin-node-resolve');
-const rollup = require('rollup-stream');
-const uglify = require('gulp-uglify');
+const rollup = require('gulp-better-rollup');
 const tap = require('gulp-tap');
+const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 
 module.exports = (config) => {
 	const {
-		src, dest, browsers,
+		src,
+		dest,
+		browsers,
 	} = config;
 
-	const es6 = () => {
-		return gulp.src(src)
-			.pipe(tap((file) => {
-				file.contents = rollup({
-					input: file.path,
-					format: 'es',
-					plugins: [
-						resolve({ browser: true }),
-						commonjs({ sourceMap: true }),
-					],
-				});
+	const pipeline = (targets, suffix) => {
+		const step1 = gulp.src(src)
+			.pipe(sourcemaps.init({
+				loadMaps: true
 			}))
-			.pipe(rename({ suffix: '.es6' }))
+			.pipe(rollup({
+				plugins: [
+					resolve({
+						browser: true
+					}),
+					commonjs({
+						sourceMap: true
+					}),
+					babel({
+						babelrc: false,
+						exclude: [
+							'node_modules/@babel/**',
+							'node_modules/core-js/**',
+							'node_modules/regenerator-runtime/**',
+						],
+						presets: [
+							[
+								"@babel/preset-env",
+								{
+									useBuiltIns: "usage",
+									ignoreBrowserslistConfig: true,
+									targets: targets,
+								}
+							]
+						]
+					}),
+				],
+			}, {
+				format: 'iife',
+			}));
+
+		const step2 = suffix ? step1.pipe(rename({
+			suffix: suffix
+		})) : step1;
+
+		return step2
+			.pipe(sourcemaps.write('./'))
 			.pipe(gulp.dest(dest))
+			.pipe(filter(['!**/*.map']))
 			.pipe(buffer())
-			.pipe(sourcemaps.init({loadMaps: true}))
-			.pipe(babili())
-			.pipe(rename({ suffix: '.min' }))
+			.pipe(sourcemaps.init({
+				loadMaps: true
+			}))
+			.pipe(minify({
+				mangle: {
+					keepFnName: true,
+					keepClassName: true
+				}
+			}))
+			.pipe(rename({
+				suffix: '.min'
+			}))
 			.pipe(sourcemaps.write('./'))
 			.pipe(gulp.dest(dest));
 	};
 
-	const babel = () => {
-		const babelSettings = {
-			presets: [
-				[babelPresetEnv, {
-					targets: {
-						browsers,
-					},
-				}],
-			],
-		};
+	const modern = () => {
+		const targets = [
+			// global coverage: 75.43% (on 21-01-2019)
+			'chrome >= 70',
+			'and_chr >= 70',
+			'ff >= 63',
+			'and_ff >= 63',
+			'safari >= 12',
+			'ios_saf >= 12',
+			'opera >= 56',
+			'Samsung >= 7',
+			'and_uc >= 11',
+		].join(', ')
+		return pipeline(targets, '.es6');
+	}
 
-		return gulp.src(src, { read: false })
-			.pipe(tap((file) => {
-				file.contents = browserify(file.path, { debug: true })
-					.transform(babelify, babelSettings)
-					.bundle();
-			}))
-			.pipe(gulp.dest(dest))
-			.pipe(buffer())
-			.pipe(sourcemaps.init({loadMaps: true}))
-			.pipe(uglify())
-			.pipe(rename({ suffix: '.min' }))
-			.pipe(sourcemaps.write('./'))
-			.pipe(gulp.dest(config.dest));
+	const legacy = () => {
+		return pipeline(browsers);
+	}
+
+	return {
+		modern,
+		legacy
 	};
-
-	return { es6, babel };
 };
